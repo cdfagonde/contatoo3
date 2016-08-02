cada87d7fbea7f6919bfe38f0620c3698faee3ad
cdfagonde <cdfagonde@hotmail.com>
2016-08-01 22:25:57 -0300

    Tirei seleniumAddress do config do protractor

--- config/protractor.js
+++ config/protractor.js
@@ -13,12 +13,11 @@ exports.config = {
 		'build' : config.travisBuild
 	},
 
-	seleniumAddress: 'http://localhost:4444/wd/hub',
-
 	specs : [
 		'../test/e2e/**/*.js'
 	],
 
+	// seleniumAddress: 'http://localhost:4444/wd/hub',
 	// chromeOnly: true,
 	// directConnect: true,
 
