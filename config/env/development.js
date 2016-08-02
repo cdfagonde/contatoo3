8fbd9c72dfac71dbf32b09ccc417ba9e86175650
cdfagonde <cdfagonde@hotmail.com>
2016-08-01 22:02:37 -0300

    Tentando de novo com o Openshift

--- config/env/development.js
+++ config/env/development.js
@@ -6,7 +6,7 @@ module.exports = {
 	clientID : process.env.CLIENT_ID,
 	clientSecret : process.env.CLIENT_SECRET,
 	seleniumUser : process.env.SELENIUM_USER,
-	seleniumUserPassword : process.env.ELENIUM_USER_PASSWORD,
+	seleniumUserPassword : process.env.SELENIUM_USER_PASSWORD,
 	port : 3000,
 	address : 'localhost',
 	domain : 'localhost'
