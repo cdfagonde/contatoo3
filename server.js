aefb80218fbba92ccdaf33aeb3128730b12052e6
cdfagonde <cdfagonde@hotmail.com>
2016-08-07 21:07:49 -0300

    Não achei muito para corrigir

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ require( './config/database' )( config.db );
 
 http.createServer( app ).listen( config.port, config.address, function() {
 	console.log( new Date());
-	console.log( 'Express Https Server ' + 
+	console.log( 'Express Http Server ' + 
 		config.address + ' (' + config.env + ') escutando na porta ' + config.port );
 });
